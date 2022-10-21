4c981ab6c14bca55dac59db0251425a9e7add71a
kong-johnny <82499045+kong-johnny@users.noreply.github.com>
2022-10-21 18:16:45 +0800

    Update SyntaxTree.c

--- byyl/2-yffx/SyntaxTree.c
+++ byyl/2-yffx/SyntaxTree.c
@@ -34,7 +34,9 @@ struct node *insNode(struct node *n, char *name, int lines, NODE_TYPE t) {
     return parent;
 }
 
-void printNode(struct node *n, FILE *f) {
+void printNode(struct node *n, FILE *f, int depth) {
+    char MID[4];
+    strcpy(MID, "Mid");
     if(n->type == STRING_TYPE) {
         fprintf(f, "%s : %s\n", n->name, n->id);
     }
@@ -45,7 +47,25 @@ void printNode(struct node *n, FILE *f) {
         fprintf(f, "FLOAT : %f\n", n->floatValue);
     }
     else {
-        fprintf(f, "%s (%d)\n", n->name, n->lines);
+        // printf("%d\n", strcmp(n->name, "Mid"));
+        if(strcmp(n->name, "Mid") == 0){
+            // printf("Yes!!");
+            fprintf(f, "Empty : Empty\n");
+            if (n->child != NULL && strcmp(n->child->name, "Stmt") == 0){
+                for(int i = 0; i < depth; i++) {
+                    fprintf(f, "\t");
+                }
+                fprintf(f, "StmtList (%d)\n", n->lines);
+            }
+            else if(n->child != NULL){
+                for(int i = 0; i < depth; i++) {
+                    fprintf(f, "\t");
+                }
+                fprintf(f, "DefList (%d)\n", n->lines);
+            }
+        }
+        else
+            fprintf(f, "%s (%d)\n", n->name, n->lines);
     }
 }
 
@@ -54,7 +74,7 @@ void printTree(struct node *head, int depth, FILE *f) {
     for(int i = 0; i < depth; i++) {
         fprintf(f, "\t");
     }
-    printNode(head, f);
+    printNode(head, f, depth);
     printTree(head->child, depth+1, f);
     printTree(head->bro, depth, f);
 }
